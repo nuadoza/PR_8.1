5e2d5b6a582e42818c11df759d5fa626e813a66e
nuadoza <144047458+nuadoza@users.noreply.github.com>
2024-06-18 13:45:27 +0300

    Add files via upload

---
+++ PR_8.1/SignUP.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR_8._1
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUP.xaml
+    /// </summary>
+    public partial class SignUP : Window
+    {
+        public SignUP()
+        {
+            InitializeComponent();
+        }
+    }
+}
